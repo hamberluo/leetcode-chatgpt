05a7d29f9c29cf6c3a6a08eaecb6d45041f84e80
hamber.luo <hamberluo@davionlabs.com>
2023-03-23 20:47:47 +0800

    fix: regen

---
+++ fix.py
@@ -0,0 +1,25 @@
+import os
+
+
+def find_files(root_dir):
+    for dir_path, dir_names, file_names in os.walk(root_dir):
+        for filename in file_names:
+            name_without_ext, ext = os.path.splitext(filename)
+            if ext == '.md' and name_without_ext.isdigit():
+                new_filename = name_without_ext.zfill(4) + ext
+                os.rename(os.path.join(dir_path, filename), os.path.join(dir_path, new_filename))
+
+
+def find_unblock_files(root_dir):
+    for dir_path, dir_names, file_names in os.walk(root_dir):
+        for filename in file_names:
+            name_without_ext, ext = os.path.splitext(filename)
+            if ext == '.md' and name_without_ext.isdigit():
+                with open(os.path.join(dir_path, filename), 'r') as f:
+                    content = f.read()
+                    if '```' not in content:
+                        print(os.path.join(dir_path, filename))
+
+
+if __name__ == '__main__':
+    find_unblock_files('.')
