b9e36357c5876ff77de63ca4f34bf32c3780f02e
hamber.luo <hamberluo@davionlabs.com>
2023-03-24 08:31:05 +0800

    feat: python solution

--- fix.py
+++ fix.py
@@ -10,16 +10,16 @@ def find_files(root_dir):
                 os.rename(os.path.join(dir_path, filename), os.path.join(dir_path, new_filename))
 
 
-def find_unblock_files(root_dir):
+def find_incomplete_files(root_dir):
     for dir_path, dir_names, file_names in os.walk(root_dir):
         for filename in file_names:
             name_without_ext, ext = os.path.splitext(filename)
             if ext == '.md' and name_without_ext.isdigit():
                 with open(os.path.join(dir_path, filename), 'r') as f:
                     content = f.read()
-                    if '```' not in content:
+                    if '```' not in content or '算法复杂度' not in content or '$' in content:
                         print(os.path.join(dir_path, filename))
 
 
 if __name__ == '__main__':
-    find_unblock_files('.')
+    find_incomplete_files('.')
